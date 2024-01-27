de79448c300543f6fe5a04e3b44b647b2cc675a6
Anna Bellec <32549757+slaphappybee@users.noreply.github.com>
2024-01-27 22:54:44 +0000

    Adding moving tiles around

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,22 @@ class Vector2 {
     constructor(public x: number, public y: number) {} 
 }
 
+class Rectangle {
+    constructor(public x: number, public y: number, public w: number, public h: number) {}
+    static from(origin: Vector2, size: Vector2) {
+        return new Rectangle(origin.x, origin.y, size.x, size.y)
+    }
+
+    contains(point: Vector2) {
+        return (
+            point.x < this.x + this.w &&
+            point.y < this.y + this.h &&
+            point.x > this.x &&
+            point.y > this.y
+        )
+    }
+}
+
 class Component {
     constructor() {
     }
@@ -50,6 +66,13 @@ class Label extends Component {
     }
 }
 
+class Dragged extends Component {
+}
+
+class Draggable extends Component {
+    // Flag for draggable tile
+}
+
 class Canvas extends Component {
     constructor(public ctx: CanvasRenderingContext2D, public canvas: HTMLCanvasElement) {
         super()
@@ -67,6 +90,12 @@ class Canvas extends Component {
     }
 }
 
+class PlayerProperties extends Component {
+    constructor(public playerName: string) {
+        super()
+    }
+}
+
 type EntityId = number
 
 type Class<T> = new (...args: any[]) => T
@@ -76,13 +105,21 @@ class World {
     registry = new Map<string, Map<number, Component>>()
     lastId = 0
 
-    private register(entityId: EntityId, component: Component) {
+    register(entityId: EntityId, component: Component) {
         const componentName = component.constructor.name
         if (!this.registry.has(componentName)) {
             this.registry.set(componentName, new Map())
         }
 
         this.registry.get(componentName)?.set(entityId, component)
+    }
+
+    rescind(entityId: EntityId, componentName: string) {
+        if (!this.registry.has(componentName)) {
+            this.registry.set(componentName, new Map())
+        }
+
+        this.registry.get(componentName)?.delete(entityId)
     }
 
     entity(components: Array<Component>): number {
@@ -117,6 +154,17 @@ class World {
         }
     }
 
+    *queryEN(primaryType: Class<Component>, otherTypes: Array<Class<Component>>): IterableIterator<Array<any>> {
+        const primaryComponents = this.registry.get(primaryType.name)
+        if (primaryComponents === undefined) {
+            return new Array().values()
+        }
+        for (const [entityId, primary] of primaryComponents.entries()) {
+            let components = [entityId, primary].concat(otherTypes.map(t => this.locate(entityId, t)))
+            yield components as Array<Optional<Component>>
+        }
+    }
+
     *queryN(primaryType: Class<Component>, otherTypes: Array<Class<Component>>): IterableIterator<Array<Optional<Component>>> {
         const primaryComponents = this.registry.get(primaryType.name)
         if (primaryComponents === undefined) {
@@ -136,6 +184,10 @@ class World {
         return this.queryN(type1 as Class<Component>, [type2 as Class<Component>, type3 as Class<Component>]) as IterableIterator<[Type1, Optional<Type2>, Optional<Type3>]>
     }
 
+    queryE3<Type1, Type2, Type3>(type1: Class<Type1>, type2: Class<Type2>, type3: Class<Type3>): IterableIterator<[EntityId, Type1, Optional<Type2>, Optional<Type3>]> {
+        return this.queryEN(type1 as Class<Component>, [type2 as Class<Component>, type3 as Class<Component>]) as IterableIterator<[EntityId, Type1, Optional<Type2>, Optional<Type3>]>
+    }
+
     queryUnique<Type>(type: Class<Type>): Type {
         const components = this.registry.get(type.name)
         if (components !== undefined) {
@@ -150,6 +202,7 @@ class System {
     onFrame(world: World, delta: number): void {}
     onMouseMove(world: World, event: MouseEvent): void {}
     onMouseUp(world: World, event: MouseEvent): void {}
+    onMouseDown(world: World, event: MouseEvent): void {}
 }
 
 class SystemRegistry extends System {
@@ -160,6 +213,12 @@ class SystemRegistry extends System {
     onFrame(world: World, delta: number): void {
         for (const system of this.systems) {
             system.onFrame(world, delta)
+        }
+    }
+
+    onMouseDown(world: World, event: MouseEvent): void {
+        for (const system of this.systems) {
+            system.onMouseDown(world, event)
         }
     }
 
@@ -186,9 +245,11 @@ class AssetStore {
 
 class GridPositionSystem extends System {
     onFrame(world: World, delta: number): void {
-        for (const [cGrid, cPosition] of world.query2(GridPosition, Position)) {
-            cPosition!.position.x = cGrid.column * 64
-            cPosition!.position.y = cGrid.line * 64
+        for (const [cGrid, cPosition, cDragged] of world.query3(GridPosition, Position, Dragged)) {
+            if (cDragged === undefined) {
+                cPosition!.position.x = cGrid.column * 64
+                cPosition!.position.y = cGrid.line * 64
+            }
         }
     }
 }
@@ -237,6 +298,46 @@ class ScrollDragSystem extends System {
     }
 }
 
+class TileDragSystem extends System {
+    private screenPosition(frames: Map<EntityId, DisplayFrame>, cPosition: Position, cPlane?: DisplayPlane): Vector2 {
+        // TODO refactor this
+        if (cPlane === undefined) {
+            return cPosition.position
+        }
+        const frameStart = frames.get(cPlane?.plane)?.innerPosition
+        return new Vector2(cPosition.position.x - frameStart!.x, cPosition.position.y - frameStart!.y)
+    }
+
+    onMouseMove(world: World, event: MouseEvent): void {
+        for (const [_, cPosition] of world.query2(Dragged, Position)) {
+            cPosition!.position.x += event.movementX
+            cPosition!.position.y += event.movementY
+        }
+    }
+
+    onMouseDown(world: World, event: MouseEvent): void {
+        const frames = new Map(world.queryE(DisplayFrame))
+
+        for (const [eid, _, cPosition, cPlane] of world.queryE3(Draggable, Position, DisplayPlane)) {
+            const position = this.screenPosition(frames, cPosition!, cPlane)
+            if (Rectangle.from(position, new Vector2(64, 64)).contains(new Vector2(event.clientX, event.clientY))) {
+                world.register(eid, new Dragged())
+            }
+        }
+    }
+
+    onMouseUp(world: World, event: MouseEvent): void {
+        const player = world.queryUnique(PlayerProperties)
+
+        for (const [eid, _, cPosition, cGrid] of world.queryE3(Dragged, Position, GridPosition)) {
+            cGrid!.column = Math.floor((cPosition!.position.x + 32) / 64)
+            cGrid!.line = Math.floor((cPosition!.position.y + 32) / 64)
+            world.rescind(eid, Dragged.name)
+            console.log(`${player.playerName} moving ${eid} to ${cGrid!.column}, ${cGrid!.line}`)
+        }
+    }
+}
+
 function main() {
     const a = new AssetStore()
     const w = new World()
@@ -254,7 +355,8 @@ function main() {
             new GridPosition(column, line),
             new DisplayPlane(frame),
             new Sprite(a.image("tile")),
-            new Label(label)
+            new Label(label),
+            new Draggable()
         ])
     }
 
@@ -265,15 +367,18 @@ function main() {
     letter(8, 5, "O")
 
     w.entity([
-        Canvas.fromId("canvas")
+        Canvas.fromId("canvas"),
+        new PlayerProperties("player1")
     ])
 
     s.systems.push(new GridPositionSystem())
     s.systems.push(new CanvasSystem())
     s.systems.push(new ScrollDragSystem())
+    s.systems.push(new TileDragSystem())
 
     addEventListener("mousemove", (event) => s.onMouseMove(w, event))
     addEventListener("mouseup", (event) => s.onMouseUp(w, event))
+    addEventListener("mousedown", (event) => s.onMouseDown(w, event))
 
     function animate() {
         requestAnimationFrame(animate)
